5e8755dba5b63246bca3723e92119b6b60155da4
dranixv12 <d.drani1234@gmail.com>
2023-03-14 11:24:12 -0600

    nun

---
+++ java lab/script.js
@@ -0,0 +1,10 @@
+const myAge = 18
+const amIAbove18 = true
+const myName = 'daniel'
+const friends = [18, true, 'daniel']
+let someSomeSpecialValue = null
+const bigIntNumber = 111111111111111111111111111111111111n
+
+const typeofMyAge = typeof myAge
+
+console.log(someSomeSpecialValue)
