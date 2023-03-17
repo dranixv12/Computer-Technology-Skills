00ffbece91d57b17e0b760418cca6e36e4096f03
dranixv12 <d.drani1234@gmail.com>
2023-03-17 11:22:14 -0600

    Update script.js

--- java lab/script.js
+++ java lab/script.js
@@ -1,10 +1,2 @@
-const myAge = 18
-const amIAbove18 = true
-const myName = 'daniel'
-const friends = [18, true, 'daniel']
-let someSomeSpecialValue = null
-const bigIntNumber = 111111111111111111111111111111111111n
-
-const typeofMyAge = typeof myAge
-
-console.log(someSomeSpecialValue)
+let myAge = 18
+console.log(myAge);
