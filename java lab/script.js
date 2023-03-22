3c162739c8e187025e6c8c7d93f21da7bfeac377
dranixv12 <d.drani1234@gmail.com>
2023-03-22 11:30:19 -0600

    vsfvf

--- java lab/script.js
+++ java lab/script.js
@@ -1,2 +1,41 @@
-let myAge = 18
-console.log(myAge);
+const myAge = 18
+let sum = 200
+let totalCount = 5
+
+function shouldIGetALicense(age){
+if (age > 18) {
+    return true
+}
+    return false
+}
+
+
+
+console.log(shouldIGetALicense(100))
+
+
+
+
+const num = 50
+function returnNumber(num){
+if (num <5) {
+return 'tiny'
+} else if (num <10){
+    return 'small'
+} else if (num <15){
+    return'medium'
+} else if (num <20){
+    return 'large'
+} else if (num >=20){
+    return 'huge'
+}
+
+}
+
+console.log(returnNumber(20000));
+
+
+
+
+function performMath(num1,num2,num3)
+
